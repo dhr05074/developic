da2b0b22e2da8f8a944f74fecff18aed21526eaf
Juchan Park <juchan@teamgrit.kr>
2023-03-28 08:30:36 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module code-connect
+
+go 1.20
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0 // indirect
+	github.com/labstack/echo/v4 v4.10.2 // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/sashabaranov/go-gpt3 v1.4.0 // indirect
+	github.com/sashabaranov/go-openai v1.5.7 // indirect
+	github.com/solywsh/chatgpt v0.0.14 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/sashabaranov/go-openai"
+	"log"
+	"os"
+)
+
+type Body struct {
+	Code string `json:"code"`
+}
+
+type ProblemBody struct {
+	Factors  []string `json:"factors"`
+	Language string   `json:"language"`
+}
+
+func main() {
+	apiKey := os.Getenv("CHATGPT_API_KEY")
+	cli := openai.NewClient(apiKey)
+	app := echo.New()
+
+	app.POST("/analyze", func(c echo.Context) error {
+		var body Body
+		if err := c.Bind(&body); err != nil {
+			return err
+		}
+
+		decoded, err := base64.StdEncoding.DecodeString(body.Code)
+		if err != nil {
+			return err
+		}
+
+		prompt := `
+Infer the programmer's five strengths and five weaknesses by the following code and convert the result to JSON format.
+
+For example:
+{
+	"strengths": [
+		"Readable and maintainable code",
+		"Good understanding of Go language"	
+	],	
+	"weaknesses": [	
+		"Complex code",	
+		"Not secure"
+	],
+	"score": 80
+}
+		`
+
+		content := fmt.Sprintf("%s\n\n%s", prompt, string(decoded))
+
+		// Analyze the code
+		response, err := cli.CreateChatCompletion(c.Request().Context(), openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo0301,
+			Messages: []openai.ChatCompletionMessage{{
+				Role:    openai.ChatMessageRoleUser,
+				Content: content,
+			}},
+		})
+		if err != nil {
+			return err
+		}
+
+		var responseMap map[string]interface{}
+		if err := json.Unmarshal([]byte(response.Choices[0].Message.Content), &responseMap); err != nil {
+			log.Println(err)
+			return err
+		}
+
+		return c.JSON(200, responseMap)
+	})
+
+	if err := app.Start(":8080"); err != nil {
+		panic(err)
+	}
+}
