b0400fe3e3ad6b02e0367bfb67a8936b97c66714
jxxh204 <ahhancom@gmail.com>
2023-06-14 17:29:06 +0900

    v0.0.0:47 -   problem nav의 언어,난이도 로컬스토리지 적용

--- front/src/recoil/problem.recoil.ts
+++ front/src/recoil/problem.recoil.ts
@@ -4,6 +4,8 @@ import { Problem } from "api/api";
 // recoil을 model로 사용한다.
 // problem 타입이 any라서 일단..
 
+// effects를 하나의 함수로 묶을려고 시도했지만 타입이 좀 까다로움.
+
 export const languageState = atom<LanguageType[]>({
     key: "languageList",
     default: ["Javascript", "Go", "Cpp"],
@@ -18,6 +20,21 @@ export const selectOptionState = atom({
         currentLang: "" as LanguageType,
         defaultDifficulty: "" as difficultyType,
     },
+    effects: [
+        ({ setSelf, onSet }) => {
+            const savedData = localStorage.getItem(selectOptionState.key);
+            // setSelf: atom 값을 설정 혹은 재설정
+            if (savedData) setSelf(JSON.parse(savedData));
+
+            // atom이 변화가 감지될 때 작동, Storage에 데이터 저장
+            // setSelf에 의해서는 작동하지 않음
+            onSet((newValue, _, isReset) => {
+                isReset
+                    ? localStorage.removeItem(selectOptionState.key)
+                    : localStorage.setItem(selectOptionState.key, JSON.stringify(newValue));
+            });
+        },
+    ],
 });
 
 export const problemIdState = atom<string | null>({
@@ -48,6 +65,7 @@ export const editorInCode = atom<string | undefined>({
     default: undefined,
 });
 
+const effectHandler = () => {};
 // recoil에서 비동기를 사용하려면 suspence를 사용해야한다.
 // export const setProblemId = selector({
 //     key: "problemId/set",
